085b331451f677c424a381998c3c2ce7f8ddfc60
JinSatba <rlcks092871@gmail.com>
2024-09-29 22:06:03 +0900

    두번째 커밋 곱셈과 나눗셈

--- calc.py
+++ calc.py
@@ -1,8 +1,11 @@
-def addition(a, b):
-    return a + b
+def multiplication(a, b):
+    return a * b
 
-def subtraction(a, b):
-    return a - b
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "0으로 나눌 수 없습니다."
 
 if __name__ == "__main__":
     a = int(input("첫 번째 숫자를 입력하세요: "))
@@ -10,3 +13,5 @@ if __name__ == "__main__":
 
     print(f"{a} + {b} = {addition(a, b)}")
     print(f"{a} - {b} = {subtraction(a, b)}")
+    print(f"{a} * {b} = {multiplication(a, b)}")
+    print(f"{a} / {b} = {division(a, b)}")
