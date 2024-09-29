3dc38623b6572179a78a6ef849f7191e0d4694fe
JinSatba <rlcks092871@gmail.com>
2024-09-29 22:03:27 +0900

    첫번째 커밋 덧셈과 뺄셈

--- calc.py
+++ calc.py
@@ -1,16 +1,12 @@
-def multiply(a, b):
-    return a * b
+def addition(a, b):
+    return a + b
 
-def divide(a, b):
-    if b == 0:
-        return "0으로 나눌 수 없습니다."
-    return a / b
+def subtraction(a, b):
+    return a - b
 
 if __name__ == "__main__":
-    x = int(input("첫 번째 숫자를 입력하세요: "))
-    y = int(input("두 번째 숫자를 입력하세요: "))
+    a = int(input("첫 번째 숫자를 입력하세요: "))
+    b = int(input("두 번째 숫자를 입력하세요: "))
 
-    print(f"숫자의 합은 {add(x, y)}입니다.")
-    print(f"{x}와 {y}의 차는: {subtract(x, y)}")
-    print(f"{x}와 {y}의 곱은: {multiply(x, y)}")
-    print(f"{x}를 {y}로 나눈 결과는: {divide(x, y)}")
+    print(f"{a} + {b} = {addition(a, b)}")
+    print(f"{a} - {b} = {subtraction(a, b)}")
