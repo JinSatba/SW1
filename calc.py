8f6ae159d25e34d133c5c867e645eec85f8ed223
JinSatba <rlcks092871@gmail.com>
2024-09-29 18:06:04 +0900

    Feature 1: 더하기 출력 형식 변경

--- calc.py
+++ calc.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     x = int(input("첫 번째 숫자를 입력하세요: "))
     y = int(input("두 번째 숫자를 입력하세요: "))
 
-    print(f"숫자의 합: {add(x, y)}")
+    print(f"Feature 1에서 수정된 결과: {add(x, y)}")
     print(f"{x}와 {y}의 차는: {subtract(x, y)}")
     print(f"{x}와 {y}의 곱은: {multiply(x, y)}")
     print(f"{x}를 {y}로 나눈 결과는: {divide(x, y)}")
